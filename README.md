37d6749c8a42a1f816064aab97e2955757bbbbf6
SANGAMESH K <sanguvk123@gmail.com>
2020-02-06 06:06:06 +0530

    greetings from url

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This is a user registration app.
+
+Step 1 : Download the Repository
+
+Step 2 : Go to microblog/app/venv/Scripts and write activate in your terminal
+
+Step 3 :  come back to the microblog directory and give the command "flask run"
+
+
+Woh your app is running now
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,18 @@
 from app import app
-
+from flask import render_template
 @app.route('/')
 @app.route('/greet')
 def greet():
     return "hello world"
+
+@app.route('/greeting')
+def greeting():
+    user = {'username': 'Sangamesh K'}
+    return render_template('index.html',title='home', user=user)
+
+@app.route('/greet/<name>')
+def hello(name):
+    return render_template('index.html',title='Home',name=name)
+
+if __name__=='__main__':
+    app.run()
