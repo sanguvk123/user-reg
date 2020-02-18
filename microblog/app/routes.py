91bb4311d64932bf0ab08b10815dc00bb9b40580
SANGAMESH K <sanguvk123@gmail.com>
2020-02-18 23:59:04 +0530

    followers

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,6 +4,8 @@ from flask import url_for
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
 from app.models import User, Post
+from app.forms import PostForm
+from app.models import Post
 from flask_login import logout_user
 from flask_login import login_required
 from flask import request
@@ -12,12 +14,26 @@ from app import db
 from datetime import datetime
 from app.forms import EditProfileForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Sangamesh K'}
-    return render_template("index.html", title='Home Page', Post=Post)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form,
+                           posts=posts)
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
 
 @app.route('/user/<username>')
 @login_required
@@ -84,6 +100,35 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
 
 @app.route('/logout')
 def logout():
